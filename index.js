9dbd802ffb594aeacdcb78dd9d40689605bebf81
Shohel Rana <ripon3334t5r@gmail.com>
2022-09-30 22:05:03 -0700

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/test',{useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => {
+const personSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        minlength:[3,'minimum 3 chars'],
+        maxlength:[10,'minimum 3 chars'],
+    },
+   lastName:String,
+   email:String,
+   age:Number,
+   bio:String,
+   single:Boolean
+})
+
+const Person = mongoose.model('Person',personSchema)
+
+mongoose.connect("mongodb+srv://ShohelRana:VD3aTF2Vkyiz6nfE@cluster0.82mknop.mongodb.net/?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true})
+.then(async() => {
     console.log('Database Connected')
+    // const person = new Person({
+    //     firstName:'Sabana',
+    //     lastName:'Jannat',
+    //     email:"ripon334t5r@gmail.com",
+    //     age:"20",
+    //     bio:"I am a Back-End Developer",
+    //     single:'true'
+    // })
+
+    // for faield validation
+    const person = new Person({firstName:'11'})
+    await person.save()
+    console.log('Person Created')
+    console.log(person)
+
+    // find
+    // const people =await person.find({lastName:'Sabana'})
+    // conosle.log(people) 
 })
 .catch((e) =>{
     console.log(e)
 })
+.finally(() => {
+    mongoose.connection.close()
+})
 
-const uri = "mongodb+srv://ShohelDeveloper:D!TwYSqeH2uyPbD@cluster0.82mknop.mongodb.net/?retryWrites=true&w=majority"
+
